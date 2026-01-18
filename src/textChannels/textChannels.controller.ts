19f910ea1e1fd08453483aaa981fbdaf54ebedef
Maciej <Ecetris@gmail.com>
2026-01-18 23:59:24 +0100

    Fixed uncaught errors in create text channels endpoint.

--- src/textChannels/textChannels.controller.ts
+++ src/textChannels/textChannels.controller.ts
@@ -83,6 +83,7 @@ export class TextChannelsController {
       if (error instanceof ChannelWithProvidedNameExistError) {
         throw new ConflictException(error.message);
       }
+      throw error;
     }
   }
 
